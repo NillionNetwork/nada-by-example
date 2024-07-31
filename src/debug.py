2728c24acced81c15296bad3e4bad737bec763d0
oceans404 <steph.orpilla@nillion.com>
2024-07-31 10:22:48 -0700

    debug by printing

--- src/debug.py
+++ src/debug.py
@@ -7,13 +7,18 @@ def nada_main():
     num_1 = SecretInteger(Input(name="num_1", party=party_alice))
     num_2 = SecretInteger(Input(name="num_2", party=party_bob))
     sum = num_1 + num_2
-    # Nada DSL does not currently support print statements and will fail if any are included
-    # instead of print(), debug by raising an exception
-    # uncomment any one of the raise Exception() lines
-    # then build this program with `nada build debug` to print the inside to the terminal
-    
-    # raise Exception(type(sum)) 
-    # raise Exception(sum) 
-    # raise Exception(party_alice) 
-    
+
+    # Debugging with print statements
+    # Comment out or remove print statements before compiling Nada programs,
+    # as compilation will fail if any print statements are included 👇
+
+    # print(num_1) # SecretInteger(inner=Input(inner=None))
+    # print(type(sum)) # <class 'nada_dsl.nada_types.types.SecretInteger'>
+    # print(sum) # SecretInteger(inner=<nada_dsl.operations.Addition object at 0x104ba5e20>)
     return [Output(sum, "sum", party_charlie)]
+
+# Include the print statements for debugging, then 
+# run this program from the root with `python3 src/debug.py`
+# to see printed values from the cli
+if __name__ == "__main__":
+    nada_main()
