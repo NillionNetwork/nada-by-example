4b3bc712e696d049ac808826884f5b77a5121ed0
Rostin <rostin.shokri@gmail.com>
2024-08-14 14:33:38 -0400

    add Newton-Ramson cube root approximation
    Co-authored-by: Lars Folkerts <lars.folkerts@gmail.com>
    Co-authored-by: Omar Atef <omaratef989@gmail.com>

---
+++ src/cube_root.py
@@ -0,0 +1,24 @@
+"""Implementation of the square root function using the Newton method
+with n iterations - set to 41 by default."""
+
+from nada_dsl import *
+import time
+
+n_iterations = 41
+def nada_main():
+
+    party1 = Party(name="Party1")
+
+    num = SecretInteger(Input(name="num", party=party1))
+    guess = num
+
+    for _ in range(n_iterations):
+        div1 = guess / Integer(3)
+        div2 = num / guess
+        div2 = div2 / guess
+        div2 = div2 / Integer(3)
+        guess = Integer(2) * div1 +  div2
+    return [Output(guess, "my_output", party1)]
+
+if __name__ == "__main__":
+   nada_main()
