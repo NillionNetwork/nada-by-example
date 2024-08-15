c82a137786d610ec581583988c13ca9bf0560998
Omar Ahmed <47993441+OmarAlmighty@users.noreply.github.com>
2024-08-15 14:39:25 -0400

    ArgMax Function (#22)
    * ArgMax function
    
    The argmax is a commonly used data wrangling operation. Efficiently implementing it from scratch is a bit non-trivial. Therefore, we want to provide this functionality as a built-in feature of nada-numpy
    
    Co-Authored-By: rostin79s <121708191+rostin79s@users.noreply.github.com>
    Co-Authored-By: lfolkerts <6434841+lfolkerts@users.noreply.github.com>
    
    * Fix yaml files
    
    In arg_max.py, the `current_indx` is set to `Integer(0)` and `one` is set to `Integer(1)`
    
    Removed the `current_indx` and `public_one` from the yaml files.
    
    Added a new line at the end of nada_project.toml file.
    
    * Fix
    
    Appended newlines
    Renaming variables
    
    Co-Authored-By: lfolkerts <6434841+lfolkerts@users.noreply.github.com>
    Co-Authored-By: rostin79s <121708191+rostin79s@users.noreply.github.com>
    
    * Removed the result index from input
    
    `result` is initialized to -1 instead of reading it as input.
    The condition is changed to be `>=` instead of `>`.
    Removed the `index` input from the test files.
    
    Co-Authored-By: lfolkerts <6434841+lfolkerts@users.noreply.github.com>
    Co-Authored-By: rostin79s <121708191+rostin79s@users.noreply.github.com>
    
    * Updated function header
    
    Co-Authored-By: lfolkerts <6434841+lfolkerts@users.noreply.github.com>
    Co-Authored-By: rostin79s <121708191+rostin79s@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: rostin79s <121708191+rostin79s@users.noreply.github.com>
    Co-authored-by: lfolkerts <6434841+lfolkerts@users.noreply.github.com>
    Co-authored-by: Dimitris Mouris <dimitris@nillion.com>

---
+++ src/arg_max.py
@@ -0,0 +1,42 @@
+from nada_dsl import *
+import nada_numpy as na
+
+DIM = 10
+
+def argmax(array: na.NadaArray):
+    # The result (the index of the argmax)
+    result = Integer(-1)
+
+    # The current index of the loop. It is set to be a public value
+    current_index = Integer(0)
+
+    # Assume the max value is at index 0
+    max_val = array[0]
+
+    # Compare the remaining content of the array with max_val
+    for v in array:
+        # Is the current value, v, greater than max_val?
+        cond = v >= max_val
+        # If true, then max_val is set to be v. Otherwise, it is not changed.
+        max_val = cond.if_else(v, max_val)
+        # If true, then result index is updated to the current index. Otherwise, it is not changed.
+        result = cond.if_else(current_index, result)
+        # Increment the index counter.
+        current_index = current_index + Integer(1)
+    return result
+
+
+def nada_main():
+    party1 = Party(name="Party1")
+    # A party to keep track of the index of the result
+    index_party = Party(name="index_party")
+
+    array = na.array([DIM], party1, "array", SecretInteger)
+
+    result = argmax(array)
+
+    return na.output(result, index_party, "argmax")
+
+if __name__ == "__main__":
+   nada_main()
+
