3ee71c3696ee75f13bcd6205d75361fe18af4258
Omar Atef <47993441+OmarAlmighty@users.noreply.github.com>
2024-08-18 13:30:50 -0400

    update toml and files' names
    Co-Authored-By: lfolkerts <6434841+lfolkerts@users.noreply.github.com>
    Co-Authored-By: rostin79s <121708191+rostin79s@users.noreply.github.com>

---
+++ src/variance_integer.py
@@ -0,0 +1,40 @@
+from nada_dsl import *
+import nada_numpy as na
+
+DIM = 10
+
+def mean(array: na.NadaArray):
+    sum = Integer(0)
+
+    # Compute the sum
+    for v in array:
+        sum += v
+
+    # Divide the sum by the number of the elements
+    m = sum / Integer(DIM)
+    return m
+
+def variance(array: na.NadaArray):
+    # Compute the mean
+    m = mean(array)
+
+    # Initialize the sum to 0
+    sum = Integer(0)
+
+    # Compute (x_i - mean)^2
+    for v in array:
+        diff = v - m
+        sum += (diff * diff)
+
+    var = sum / Integer(DIM)
+    return  var
+
+def nada_main():
+    party1 = Party(name="Party1")
+
+    arr = na.array([DIM], party1, "array", SecretInteger)
+    
+
+    var = variance(arr)
+
+    return na.output(var, party1, "variance")
