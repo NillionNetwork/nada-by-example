dacfec1a2a40d831e36749fa6b775ed3504f8af3
oceans404 <steph.orpilla@nillion.com>
2024-08-27 17:37:42 -0700

    use nucleus config

--- streamlit_demo_apps/app_nucleus_multiplication.py
+++ streamlit_demo_apps/app_nucleus_multiplication.py
@@ -6,6 +6,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import streamlit_app  
 
 def main():
+    # compiled with nucleus latest-experimental nillion -V 
+    # tools-config 668efb7426c02c0e7c037d84157d08a524871fbd
     program_name = 'multiplication'
     program_test_name = 'multiplication_test'
 
