ac52a6b39da85dff0dc8a35a5a5a23f1ac69d36b
oceans404 <steph.orpilla@nillion.com>
2024-10-10 10:45:34 -0700

    add variable for num_bidders

--- src/auction_can_tie.py
+++ src/auction_can_tie.py
@@ -5,16 +5,17 @@ def nada_main():
     auctioneer = Party(name="auctioneer")
     
     # Create a list of 5 bidders participating in the auction, 'bidder_0' to 'bidder_4'
-    bidders = [Party(name=f"bidder_{i}") for i in range(5)]
+    num_bidders = 5
+    bidders = [Party(name=f"bidder_{i}") for i in range(num_bidders)]
     
     # Collect bids from each bidder, where each bid is a SecretInteger input unique to the respective bidder
-    bids = [SecretInteger(Input(name=f"bid_{i}", party=bidders[i])) for i in range(5)]
+    bids = [SecretInteger(Input(name=f"bid_{i}", party=bidders[i])) for i in range(num_bidders)]
     
     # Determine the highest bid among all the bidders
     # Start by initializing the highest bid to the first bidder's bid
     highest_bid = bids[0]
     # Iterate through the remaining bids to update the highest bid if a higher one is found
-    for i in range(1, 5):
+    for i in range(1, num_bidders):
         is_higher = bids[i] > highest_bid
         highest_bid = is_higher.if_else(bids[i], highest_bid)
 
@@ -22,7 +23,7 @@ def nada_main():
     # Each output is a flag (1 if the bid matches the highest bid, 0 otherwise), visible to the auctioneer
     placed_highest_bid = [
         Output((bids[i] == highest_bid).if_else(Integer(1), Integer(0)), f"bidder_{i}_placed_highest_bid", auctioneer)
-        for i in range(5)
+        for i in range(num_bidders)
     ]
 
     # Return the highest bid and a list of flags indicating which bidders placed the highest bid
