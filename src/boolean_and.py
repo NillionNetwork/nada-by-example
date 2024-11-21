5e92abbdc1d8fb28f766bc344b97ba175f2d4001
Juan M Salamanca <juan.salamanca@nillion.com>
2024-11-21 21:40:01 +0000

    feat: add examples for boolean operators

---
+++ src/boolean_and.py
@@ -0,0 +1,14 @@
+from nada_dsl import *
+from nada_dsl.future import *
+
+
+def nada_main():
+    party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
+    A = SecretInteger(Input(name="A_neg", party=party1))
+    B = SecretInteger(Input(name="B_neg", party=party2))
+    C = SecretInteger(Input(name="C", party=party2))
+
+    result = (A < (B + C)) & (A < C)
+
+    return [Output(result, "my_output", party1)]
